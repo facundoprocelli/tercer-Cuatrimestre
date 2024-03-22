c01398c84fb917d94c0b9a700f7e7971b1c1f54c
Facundo Procelli <facundoprocelli@gmail.com>
2024-03-22 09:11:17 -0300

    Explicación de clases con lapices

--- Clases/src/Main.java
+++ Clases/src/Main.java
@@ -1,4 +1,5 @@
 import Clases.Persona;
+import modelo.Lapiz;
 
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class Main {
         //Declaro el Scanner
         Scanner scan = new Scanner(System.in);
 
+        /*
         // Declaro un arreglo de personas
         Persona[] person = new Persona[3];
 
@@ -39,5 +41,35 @@ public class Main {
             System.out.println("El nombre es: " + person[i].getNombre() + " El genero es " + person[i].getGenero() + " La altura es " + person[i].getAltura());
 
         }
+    */
+
+        // llamo a los constructores y el compliador elije cual es el mejor
+        Lapiz lapiz001 = new Lapiz("azul");
+        Lapiz lapiz002 = new Lapiz("rojo");
+        Lapiz lapiz003 = new Lapiz();
+
+
+
+        // Las modificacións que yo hago aca solo afectan a ese lapiz
+        lapiz001.setMarca("Faber");
+        //System.out.println(lapiz001.getInfo());
+
+        mostrarLapices(lapiz001, lapiz002,lapiz003);
+        mostrarLapices(lapiz001, lapiz002,lapiz003);
     }
+
+    // Creo esta funcion en donde yo puedo mostrar todos los lapices
+    private static void mostrarLapices(Lapiz _lapiz001, Lapiz lapiz002, Lapiz lapiz003) {
+        System.out.println(_lapiz001.getInfo());
+        System.out.println(lapiz002.getInfo());
+        System.out.println(lapiz003.getInfo());
+
+        //Aca no se esta haciendo el cambio en una copia sino que
+        //Se hace el cambio como si fueran punters
+        // En java todos son punteros
+        _lapiz001.setColor("Rojo");
+
+    }
+
+
 }
--- Clases/src/modelo/Lapiz.java
+++ Clases/src/modelo/Lapiz.java
@@ -10,11 +10,14 @@ public class Lapiz {
 
     // Constructor vacio
     public Lapiz() {
-        color = "";
+        color = "sin color";
         largo  = 0;
         marca = "Sin marca";
     }
 
+    public Lapiz(String p_color){
+        this.color = p_color;
+    }
 
     // Constructor Completo
     public Lapiz(String p_color, int  p_largo, String p_marca) {
@@ -22,4 +25,37 @@ public class Lapiz {
         this.largo = p_largo;
         this.marca = p_marca;
     }
+
+
+    // Getters y Setters
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getInfo(){
+
+        return "El modelos es " + getMarca()+ " el color es " + getColor() + " y el largo es " + getLargo();
+    }
+
 }
